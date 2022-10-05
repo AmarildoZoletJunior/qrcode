c6e8f97985dc38d825b72a8ad144fc2edf8919eb
Amarildo Antonio Zolet Junior <amarildozj@gmail.com>
2022-10-05 12:13:00 -0300

    Adicionando validação nos campos

--- lib/TelaPrincipal.dart
+++ lib/TelaPrincipal.dart
@@ -10,16 +10,20 @@ class MyApp1 extends StatefulWidget {
 class tela1 extends State<MyApp1> {
 
   final myController = TextEditingController();
+  final number = TextEditingController();
   // This widget is the root of your application.
+
+  final _formKey = GlobalKey<FormState>();
   @override
 
   void abrirSegundaTela(){
     String link;
+    int num;
 
     setState(() {
       link = myController.text;
-
-      Navigator.push(context, MaterialPageRoute(builder: (context) => Qrcode(link)));
+      num = int.parse(number.text);
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Qrcode(link,num)));
     });
   }
 
@@ -27,45 +31,83 @@ class tela1 extends State<MyApp1> {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset("./imagens/logo.png",color: Colors.red,),
-            SafeArea(child: Center(child: Text("Seja bem vindo ao gerador de QRCODE, aqui você pode gerar um QRcode de tudo que você quiser, basta apenas colar qualquer coisa abaixo",
-              textAlign: TextAlign.center,style: TextStyle(fontSize: 15),))),
-            SafeArea(
-              child: Center(
-                child: Container(
-                  width: 320,
-                  child: TextFormField(
-                    controller: myController,
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.qr_code),
-                      hintText: 'Digite o que quiser',
-                      labelText: 'Cole aqui seu link',
-                    ) ,
+        body: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset("./imagens/logo.png",color: Colors.red,width: 250,),
+              SafeArea(child: Center(child: Text("Seja bem vindo ao gerador de QRCODE, aqui você pode gerar um QRcode de tudo que você quiser, basta apenas colar qualquer coisa abaixo",
+                textAlign: TextAlign.center,style: TextStyle(fontSize: 18),))),
+              SafeArea(child: Center(child: Text("***Tenha muito cuidado com o campo de numero, ele irá definir o tamanho do seu QRCode",
+                textAlign: TextAlign.center,style: TextStyle(fontSize: 15,color: Colors.redAccent),))),
+              SafeArea(
+                child: Center(
+                  child: Container(
+                    width: 320,
+                    child: TextFormField(
+                      validator: (value){
+                        if(value.toString().length < 1){
+                          return 'Este campo necessita de pelo menos 2 caracteres';
+                        }
+                      },
+                      controller: myController,
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.qr_code),
+                        hintText: 'Digite o que quiser',
+                        labelText: 'Cole aqui seu link',
+                      ) ,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Container(
-                margin: EdgeInsets.only(top: 30),
-                width: 270,
-                height: 80,
-
-                child: ElevatedButton(
-                  child: Text('Criar QRCODE'),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.teal,
-                    onPrimary: Colors.white,
-                    onSurface: Colors.grey,
+              SafeArea(
+                child: Center(
+                  child: Container(
+                    width: 320,
+                    child: TextFormField(
+                      validator: (value){
+                        if(value.toString().length < 1){
+                          return 'Este campo necessita de pelo menos 2 caracteres';
+                        }
+                        int teste = int.parse(value.toString());
+                        final b = RegExp(r'^[0-9]+$');
+                        if(b.hasMatch(value.toString()) == false){
+                          return "Este campo não pode conter letras";
+                        }
+                          if(teste < 1 || teste > 40){
+                            return "Este campo so pode conter numero entre 1 e 40";
+                          }
+                      },
+                      controller: number,
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.numbers),
+                        hintText: 'Digite um numero',
+                        labelText: 'Digite um numero para criar um qrcode',
+                      ) ,
+                    ),
                   ),
-                  onPressed: () {
-                    abrirSegundaTela();
-                  },
-                ))
-          ],
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.only(top: 30),
+                  width: 230,
+                  height: 60,
+
+                  child: ElevatedButton(
+                    child: Text('Criar QRCODE'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.teal,
+                    ),
+                    onPressed: () {
+                      if(_formKey.currentState!.validate()){
+                        abrirSegundaTela();
+                      }
+                    },
+                  )),
+            ],
+          ),
         ),
       ),
     );
--- lib/telaSucesso.dart
+++ lib/telaSucesso.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
+import 'TelaPrincipal.dart';
+
 class Qrcode extends StatefulWidget {
 
   String nome;
-  Qrcode(this.nome);
+  int num;
+  Qrcode(this.nome,this.num);
 
   @override
   State<Qrcode> createState() => _QrcodeState();
@@ -15,16 +18,23 @@ class _QrcodeState extends State<Qrcode> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+       leading: IconButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp1()));}, icon: Icon(Icons.home),)
+        ),
         body: Container(
         color: Colors.white,
         child:   Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             SafeArea(child: Text("Codigo gerado com sucesso!!",textAlign: TextAlign.center,style: TextStyle(fontSize:35))),
             SafeArea(
               child: Center(
                 child: QrImage(
   data: widget.nome,
-                  version: 3,
+                  version: widget.num,
+                  size: 250.0,
                   foregroundColor: Colors.red,
                 ),
               ),
